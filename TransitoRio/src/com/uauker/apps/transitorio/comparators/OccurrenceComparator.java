0b4bf1043d51b442933ae63186be3014bdace52a
Alexandre Santos <alexandre.marones@gmail.com>
2013-08-18 20:16:09 -0300

    criando comparator

---
+++ TransitoRio/src/com/uauker/apps/transitorio/comparators/OccurrenceComparator.java
@@ -0,0 +1,30 @@
+package com.uauker.apps.transitorio.comparators;
+
+import java.util.Comparator;
+
+import android.annotation.SuppressLint;
+
+import com.uauker.apps.transitorio.models.Occurrence;
+
+public class OccurrenceComparator implements Comparator<Occurrence> {
+
+	@SuppressLint("DefaultLocale")
+	@Override
+	public int compare(Occurrence lhs, Occurrence rhs) {
+		String directionNameA = lhs.roadDirectionName.toLowerCase();
+		String directionNameB = rhs.roadDirectionName.toLowerCase();
+
+		int compared = directionNameA.compareTo(directionNameB);
+
+		if (compared == 0 && lhs.distanceMarkerIni > rhs.distanceMarkerIni) {
+			return 1;
+		}
+
+		if (compared == 0 && lhs.distanceMarkerIni < rhs.distanceMarkerIni) {
+			return -1;
+		}		
+		
+		return compared;
+	}
+
+}
