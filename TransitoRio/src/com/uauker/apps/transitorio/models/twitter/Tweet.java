fe6095ec4b58cfd8b09eca5da37a656fcb94462b
Paulo Guilherme <uauker@gmail.com>
2013-08-19 00:36:25 -0300

    parse do telefone e serializable

--- TransitoRio/src/com/uauker/apps/transitorio/models/twitter/Tweet.java
+++ TransitoRio/src/com/uauker/apps/transitorio/models/twitter/Tweet.java
@@ -1,9 +1,15 @@
 package com.uauker.apps.transitorio.models.twitter;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Tweet {
+public class Tweet implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7953009711285241303L;
+	
 	public TwitterUser user;
 	public Date publishDate;
 	public String text;
