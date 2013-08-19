a800137a822cbdeb0596dd624180776d59621fa0
Paulo Guilherme <uauker@gmail.com>
2013-08-18 23:15:20 -0300

    chaves do twitter

--- TransitoRio/src/com/uauker/apps/transitorio/helpers/ConfigHelper.java
+++ TransitoRio/src/com/uauker/apps/transitorio/helpers/ConfigHelper.java
@@ -7,6 +7,11 @@ public class ConfigHelper {
 
 	public final static String CHARSET_DEFAULT = "UTF-8";	
 	
+	public final static String TWITTER_CONSUMER_KEY = "WPDngmoDgU7SgRinKG5A";
+	public final static String TWITTER_CONSUMER_SECRET = "2fbdQ69bdHAkhRLsrLKjj9gvdtdxJsKB7jgosO45nE";
+	public final static String TWITTER_ACCESS_TOKEN = "25685569-SSMXlJ5SQqQ56u1DuedEDyikdQG9Uxc6y6hWGJ7gg";
+	public final static String TWITTER_ACCESS_TOKEN_SECRET = "Vuc8NqLOwFXLVcKGphKKuh3XZnc8M5vEj61VQMEpWQ";
+	
 	public final static String SOURCE_URL = "http://www.%s.com.br/generic/home/ListOccurrences";
 	
 	public static String urlFormat(String... values) {
