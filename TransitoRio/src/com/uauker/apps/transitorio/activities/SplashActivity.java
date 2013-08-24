dcba74432a6bfa7ff5e9fc11003437625758e950
Paulo Guilherme <uauker@gmail.com>
2013-08-23 22:02:04 -0300

    incluido bug sense

--- TransitoRio/src/com/uauker/apps/transitorio/activities/SplashActivity.java
+++ TransitoRio/src/com/uauker/apps/transitorio/activities/SplashActivity.java
@@ -4,12 +4,14 @@ import roboguice.activity.RoboSplashActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.bugsense.trace.BugSenseHandler;
 import com.uauker.apps.transitorio.R;
 
 public class SplashActivity extends RoboSplashActivity {
 
 	@Override
 	public void onCreate(Bundle bundle) {
+		BugSenseHandler.initAndStartSession(this, "5e8d9705");
 		setContentView(R.layout.activity_splash);
 
 		super.onCreate(bundle);
